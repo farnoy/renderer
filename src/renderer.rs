ad3bb437f78bd0d18926b5b96fc97a61703a4666
Jakub Okoński <jakub@okonski.org>
2020-04-26 16:53:35 +0200

    Remove blend state from Depth pass pipeline

--- src/renderer.rs
+++ src/renderer.rs
@@ -605,14 +605,8 @@ impl DepthPassData {
                 .color_blend_state(
                     &vk::PipelineColorBlendStateCreateInfo::builder()
                         .attachments(&[vk::PipelineColorBlendAttachmentState {
-                            blend_enable: 1,
-                            src_color_blend_factor: vk::BlendFactor::SRC_ALPHA,
-                            dst_color_blend_factor: vk::BlendFactor::ONE_MINUS_SRC_ALPHA,
-                            color_blend_op: vk::BlendOp::ADD,
-                            src_alpha_blend_factor: vk::BlendFactor::ONE_MINUS_SRC_ALPHA,
-                            dst_alpha_blend_factor: vk::BlendFactor::ZERO,
-                            alpha_blend_op: vk::BlendOp::ADD,
-                            color_write_mask: vk::ColorComponentFlags::all(),
+                            blend_enable: 0,
+                            ..vk::PipelineColorBlendAttachmentState::default()
                         }])
                         .build(),
                 )
